942998edb017574db97a8eb73c2268b9d5c925be
Bruno Oliveira <bruno@abstractj.org>
2012-05-08 02:41:14 -0300

    Adjusting assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,10 +15,12 @@ KitchensinkTorqueboxMobile::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.assets.paths << "#{Rails.root}/app/assets/stylesheets/css"
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
