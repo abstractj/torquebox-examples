20e5882e48bb7532c2ebb7b5300d6eadcf2764c8
Bruno Oliveira <bruno@abstractj.org>
2012-05-08 02:29:29 -0300

    Including new assets

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,7 @@ module KitchensinkTorqueboxMobile
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.paths << "#{Rails.root}/app/assets/stylesheets/css"
   end
 end
